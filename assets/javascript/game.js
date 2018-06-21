ba4752ef9643054d13b38915970f78c22ec1af49
Will Smith <16091377+willeo@users.noreply.github.com>
2018-06-20 19:14:33 -0500

    hhhmmm...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,23 +10,20 @@ var words = [
 var word = words[Math.floor(Math.random() * words.length)];
 
 var score = 0;
-var userText;
 var hideTxt = word.length;
+
 var answerArray = [];
 for (var i = 0; i < word.length; i++) {
+  console.log(answerArray);
   answerArray[i] = "_";
   document.getElementById("answer").innerHTML = answerArray.join(" ");
 }
-var guess = userText;
+
 // Take user's input and check if it's in the Array.
 
 for (var i = 0; i < word.length; i++) {
   if (userText === word[i]) {
     answerArray[i] = guess;
-  }
-  else (guess !== word[i]) {
-    score++;
-    document.getElementById("score").innerHTML = score;
   }
 }
 
