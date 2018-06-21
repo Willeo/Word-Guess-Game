e1a3abc2c8698c5554baf618a19b1abeb3a855a4
Will Smith <16091377+willeo@users.noreply.github.com>
2018-06-20 19:00:19 -0500

    another save.. problem with the for loop that's breaking stuff.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,7 +10,6 @@ var words = [
 var word = words[Math.floor(Math.random() * words.length)];
 
 var score = 0;
-var simpsons;
 var userText;
 var hideTxt = word.length;
 var answerArray = [];
@@ -18,20 +17,20 @@ for (var i = 0; i < word.length; i++) {
   answerArray[i] = "_";
   document.getElementById("answer").innerHTML = answerArray.join(" ");
 }
-
+var guess = userText;
 // Take user's input and check if it's in the Array.
-while (hideTxt < 0) {
-  document.getElementById("answer").innerHTML = answerArray.join(" ");
-}
 
 for (var i = 0; i < word.length; i++) {
-  if (word[i] === userText) {
-    answerArray[i] = userText;
+  if (userText === word[i]) {
+    answerArray[i] = guess;
+  }
+  else (guess !== word[i]) {
+    score++;
+    document.getElementById("score").innerHTML = score;
   }
 }
 
 confirm("Wanna play??");
-
 // user input selection
 var userText = document.getElementById("user-text");
 document.onkeyup = function(event) {
