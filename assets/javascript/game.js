552f79f64421fa2706ffbfacb2369ac497021023
Will Smith <16091377+willeo@users.noreply.github.com>
2018-06-20 19:41:44 -0500

    It works!! =D

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,26 +10,32 @@ var words = [
 var word = words[Math.floor(Math.random() * words.length)];
 
 var score = 0;
-var hideTxt = word.length;
-
+var remainingLetters = word.length;
 var answerArray = [];
 for (var i = 0; i < word.length; i++) {
-  console.log(answerArray);
   answerArray[i] = "_";
   document.getElementById("answer").innerHTML = answerArray.join(" ");
 }
 
 // Take user's input and check if it's in the Array.
 
-for (var i = 0; i < word.length; i++) {
-  if (userText === word[i]) {
-    answerArray[i] = guess;
-  }
-}
-
 confirm("Wanna play??");
 // user input selection
 var userText = document.getElementById("user-text");
 document.onkeyup = function(event) {
   userText.textContent = event.key;
+  console.log(userText.textContent);
+  console.log(word);
+  console.log(answerArray);
+  var guess = userText.textContent;
+
+  for (var i = 0; i < word.length; i++) {
+    if (guess === word[i]) {
+      answerArray[i] = guess;
+      remainingLetters--;
+      document.getElementById("answer").innerHTML = answerArray.join(" ");
+    }
+  }
 };
+
+//here the user guesses and the array should print on the screen
